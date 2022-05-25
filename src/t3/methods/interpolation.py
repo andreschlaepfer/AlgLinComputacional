392cf6762df792090906099f1c197e1898fd741f
Andre Schlaepfer <aaschlaepfer@inoa.com.br>
2022-05-25 17:45:12 -0300

    Add: interpolation

---
+++ src/t3/methods/interpolation.py
@@ -0,0 +1,57 @@
+from asyncio.windows_events import NULL
+from re import L
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import *
+from pprint import pprint
+from numpy import array, zeros, diag, diagflat, dot, linalg
+from copy import deepcopy
+from src.utils.operations import *
+
+class Interpolation:
+    def __init__(self, points):
+        self.points = points
+        self.n = len(self.points)
+        self.m = self.n -1
+        self.x = [i[0] for i in self.points]
+        self.y = [i[1] for i in self.points]
+        self.xplt = np.linspace(self.get_x()[0], self.get_x()[-1])
+        self.yplt = np.array([], float)
+        self.xyplt = self.get_xyplt()
+    
+    def get_x(self):
+        return np.array([i[0] for i in self.points], float)
+    def get_y(self):
+        return np.array([i[1] for i in self.points], float)
+    def get_xyplt(self):
+        return [self.xplt, self.yplt]
+    
+
+    def get_lagrangian_interpolation_yp_from_xp(self, xp):
+        yp = 0
+        for i in range(self.n + 1):
+            p = 1
+            for j in range(self.n + 1):
+                if j != i:
+                    p *= (xp - self.x[j]) / (self.x[i] - self.x[j])
+            yp += self.y[i]*p
+        self.points.append([xp, yp])
+        return xp, yp
+    
+    def get_full_lagrangian_interpolation(self):
+        for xp in self.xplt:
+            yp = 0
+            for xi, yi in zip(self.get_x(), self.get_y()):
+                yp += yi * np.prod((xp - self.get_x()[self.get_x() != xi]) / (xi - self.get_x()[self.get_x() != xi]))
+            self.yplt = np.append(self.yplt, yp)
+
+
+    def show_graph(self, full = True):
+        if (full == False):
+            plt.plot(self.get_x, self.get_y, 'ro')
+        else:
+            plt.plot(self.get_x, self.get_y, 'ro', self.xplt, self.yplt, 'b-')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.show()
